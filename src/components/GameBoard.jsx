c3d6da861620981c3f72fe4d18261034c74a4b35
StevieBrooks <brookscodeblog@gmail.com>
2023-11-06 08:58:19 +0700

    first commit for new game

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,22 @@
+import GameCell from "./GameCell"
+import { useContext, useState } from "react"
+import { GameState } from "../ContextFile"
+
+export default function GameBoard() {
+
+    const [player, setPlayer, boardState, setBoardState, result, setResult] = useContext(GameState)
+
+    return (
+
+        <div className="game-board w-96 h-96 bg-slate-600 m-auto">
+            {boardState.map((cell, index) => (
+                cell.map((gameCell, index) => (
+                    <GameCell key={index} />
+                ))
+            ))}
+        </div>
+
+    )
+}
+
+// need to figure out how to arrange cells
---
+++ src/components/GameCell.jsx
@@ -0,0 +1,5 @@
+export default function GameCell() {
+    return (
+        <div className="game-cell w-28 h-28 m-1 bg-slate-400"></div>
+    )
+}
